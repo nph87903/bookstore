1ac26064007835088904abb5e85768943eacfbb9
Penghai <nph87903@gmail.com>
2012-04-22 17:02:05 -0300

    confidence and support changed to number

--- database/新建文本文档.sql
+++ database/新建文本文档.sql
@@ -1,71 +1,74 @@
 create table book
 (
-  book_id           NUMBER,
-  title        VARCHAR(100),
+book_id  NUMBER,
+title		VARCHAR(100),
 author	VARCHAR(100),
-  price        VARCHAR(100),
+price		VARCHAR(100),
 ISBN		VARCHAR(100),
-category_id number,
-publisher varchar(100),
-year  varchar(10),
-imagename varchar(100),
-  description VARCHAR(2000),
- primary key(book_id)
+category_id		number,
+publisher	varchar(100),
+year		varchar(10),
+imagename		varchar(100),
+description		VARCHAR(2000),
+primary	key(book_id),
+foreign key(category_id) references category(category_id)
 )
 
 create table category
-£¨
-category_id number,
-category_name varchar(100),
-primary key(category_id),
-
-
-£©
+ï¼ˆ
+category_id		number,
+category_name	varchar(100),
+primary	key(category_id)
+ï¼‰
 
 create table bestseller
 (
-  book_id           NUMBER,
-  title        VARCHAR(100),
+book_id	NUMBER,
+title		VARCHAR(100),
 author	VARCHAR(100),
-  price        VARCHAR(100),
+price		VARCHAR(100),
 ISBN		VARCHAR(100),
-category_id number,
-publisher varchar(100),
-year  varchar(10),
-imagename varchar(100),
-  description VARCHAR(2000),
- primary key(book_id)
+category_id		number,
+publisher		varchar(100),
+year		varchar(10),
+imagename		varchar(100),
+description		VARCHAR(2000),
+primary key(book_id),
+foreign key(book_id) references book(book_id),
+foreign key(category_id) references category(category_id)
 )
 
 create table recommend
 (
-  book_id           NUMBER,
-  title        VARCHAR(100),
+book_id          NUMBER,
+title        VARCHAR(100),
 author	VARCHAR(100),
-  price        VARCHAR(100),
+price        VARCHAR(100),
 ISBN		VARCHAR(100),
 category_id number,
 publisher varchar(100),
 year  varchar(10),
 imagename varchar(100),
-  description VARCHAR(2000),
- primary key(book_id)
+description VARCHAR(2000),
+primary key(book_id),
+foreign key(book_id) references book(book_id)
 )
 
 create table specialproducts
 (
-  book_id           NUMBER,
-  title        VARCHAR(100),
+book_id           NUMBER,
+title        VARCHAR(100),
 author	VARCHAR(100),
-  price        VARCHAR(100),
+price        VARCHAR(100),
 specialprice varchar(100),
 ISBN		VARCHAR(100),
 category_id number,
 publisher varchar(100),
 year  varchar(10),
 imagename varchar(100),
-  description VARCHAR(2000),
- primary key(book_id)
+description VARCHAR(2000),
+primary key(book_id),
+foreign key(book_id) references book(book_id)
 )
 
 create table users
@@ -73,7 +76,7 @@ create table users
 username varchar(100),
 password varchar(100),
 email	varchar(100),
-primary key(username)
+primary key(email)
 )
 
 create table admin
@@ -93,11 +96,43 @@ message varchar(100),
 primary key(email)
 )
 
+create table basket (
+book_id number, 
+order_id number,
+primary key (book_id,order_id),
+foreign key(book_id) references book(book_id),
+foreign key(order_id) references orders(order_id)
+)
 
 create table orders
 (
+order_id number AUTO_INCREMENT,
+email varchar(100),
 dates varchar(100),
 items varchar(1000),
 address varchar(1000),
-primary key(dates)
+primary key(order_id),
+foreign key(email) references users(email)
 )
+
+create table Coupons (
+code varchar(20) primary key,
+reward number(5))
+
+
+create table Params (
+support number(4,2) primary key, 
+confidence number(4,2),
+num_transactions number(4)
+)
+
+create table AssociationRules(
+itemX varchar(100),
+itemY varchar(100),
+support number,
+confidence number,
+primary key(itemX, itemY)
+)
+
+
+
